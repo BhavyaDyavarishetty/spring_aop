5511162f3e12be9d61dbb5548e6cd6c8759cba2c
Bhavya Dyavarishetty <Bhavya Dyavarishetty>
2019-03-14 12:54:30 -0700

    Update SpringAopApplication.java
    Remove component scan

--- src/main/java/com/bhavya/springaop/SpringAopApplication.java
+++ src/main/java/com/bhavya/springaop/SpringAopApplication.java
@@ -2,14 +2,12 @@ package com.bhavya.springaop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableWebMvc
-@ComponentScan(basePackages = { "com.bhavya.springaop.controller" })
 public class SpringAopApplication {
 
 	public static void main(String[] args) {
