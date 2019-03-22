ccda6f937cac544c1b624c832f9c8f58260aeaf0
Bhavya Dyavarishetty <bhavya.shankar01@gmail.com>
2019-03-22 15:26:47 -0700

    Added Before annotation

--- src/main/java/com/bhavya/springaop/aop/LoggerAspect.java
+++ src/main/java/com/bhavya/springaop/aop/LoggerAspect.java
@@ -4,13 +4,17 @@ import com.bhavya.springaop.conversion.LoggerEntityConverter;
 import com.bhavya.springaop.dto.LoggerTypeEnum;
 import com.bhavya.springaop.entity.LoggerEntity;
 import com.bhavya.springaop.repository.LoggerRepository;
+import com.sun.deploy.util.StringUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -19,6 +23,17 @@ public class LoggerAspect {
   @Autowired private LoggerRepository loggerRepository;
 
   @Autowired private LoggerEntityConverter loggerEntityConverter;
+
+  @Before("@annotation(com.bhavya.springaop.aop.Logger)")
+  public void beforeMethod(JoinPoint joinPoint) {
+    MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+    String methodName =  signature.getMethod().getName();
+    String className = signature.getMethod().getDeclaringClass().getName();
+    String args = StringUtils.join(Arrays.asList(joinPoint.getArgs()), ",");
+
+    LoggerEntity loggerEntity = loggerEntityConverter.convert(args, methodName, className, LoggerTypeEnum.LOG);
+    loggerRepository.save(loggerEntity);
+  }
 
   @AfterReturning(pointcut = "@annotation(com.bhavya.springaop.aop.Logger)", returning = "result")
   public void afterReturnMethod(JoinPoint joinPoint, Object result) {
